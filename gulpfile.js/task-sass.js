1ab09b7caf193738d82e6920bb9f2489c2379704
Takuto Yanagida <yanagida@stxst.com>
2021-10-14 11:18:08 +0900

    Update gulp process

--- gulpfile.js/task-sass.js
+++ gulpfile.js/task-sass.js
@@ -3,7 +3,7 @@
  * Function for gulp (SASS)
  *
  * @author Takuto Yanagida
- * @version 2021-09-26
+ * @version 2021-10-13
  *
  */
 
@@ -12,12 +12,13 @@
 const SASS_OUTPUT_STYLE = 'compressed';  // 'expanded' or 'compressed'
 
 const gulp = require('gulp');
-const $    = require('gulp-load-plugins')({ pattern: ['gulp-*'] });
+const sass = require('gulp-sass')(require('sass'));
+const $ = require('gulp-load-plugins')({ pattern: ['gulp-*', '!gulp-sass'] });
 
 function makeSassTask(src, dest = './dist', base = null, addPostfix = true) {
 	const sassTask = () => gulp.src(src, { base: base, sourcemaps: true })
 		.pipe($.plumber())
-		.pipe($.dartSass({ outputStyle: SASS_OUTPUT_STYLE }))
+		.pipe(sass({ outputStyle: SASS_OUTPUT_STYLE }))
 		.pipe($.autoprefixer({ remove: false }))
 		.pipe($.rename({ extname: addPostfix ? '.min.css' : '.css' }))
 		.pipe($.changed(dest, { hasChanged: $.changed.compareContents }))
