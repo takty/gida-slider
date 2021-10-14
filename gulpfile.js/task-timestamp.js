b89618618071c9c5f78dbe58bfec17e0e6966b3c
Takuto Yanagida <yanagida@stxst.com>
2021-10-14 14:25:32 +0900

    Improve gulp tasks

--- gulpfile.js/task-timestamp.js
+++ gulpfile.js/task-timestamp.js
@@ -18,7 +18,7 @@ function makeTimestampTask(src, dest = './dist', base = null) {
 	const timestampTask = () => gulp.src(src, { base: base })
 		.pipe($.plumber())
 		.pipe($.ignore.include({ isFile: true }))
-		.pipe($.replace(/v\d+t/g, `v${moment().format('HHmmss')}t`))
+		.pipe($.replace(/v\d+t/g, `v${moment()().format('HHmmss')}t`))
 		.pipe($.changed(src, { hasChanged: $.changed.compareContents }))
 		.pipe(gulp.dest(dest));
 	return timestampTask;
