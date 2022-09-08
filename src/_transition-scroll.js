0847ce93a7134fe612540b7143b17c3467ce5e34
Takuto Yanagida <yanagida@stxst.com>
2022-09-09 00:32:36 +0900

    Fix scroll transition again

--- src/_transition-scroll.js
+++ src/_transition-scroll.js
@@ -2,7 +2,7 @@
  * Scroll Transition
  *
  * @author Takuto Yanagida
- * @version 2022-09-07
+ * @version 2022-09-09
  */
 
 
@@ -13,9 +13,10 @@ class TransitionScroll {
 		this._sls  = slides;
 		this._time = tranTime;
 
-		this._cur   = 0;
-		this._doing = false;
-		this._queue = [];
+		this._cur    = 0;
+		this._curPsd = 0;
+		this._doing  = false;
+		this._queue  = [];
 
 		const ps = this._calcPosition(0, 1);
 		for (let i = 0; i < this._sls.length; i += 1) {
@@ -25,76 +26,85 @@ class TransitionScroll {
 	}
 
 	async transition(idx, dir) {
-		this._queue.push(cur => this._doTransition(cur, idx, dir));
+		this._queue.push((cur, curPsd) => this._doTransition(cur, curPsd, idx, dir));
 		if (this._doing) return;
 		this._doing = true;
 		while (this._queue.length) {
-			this._cur = await this._queue.shift()(this._cur);
+			[this._cur, this._curPsd] = await this._queue.shift()(this._cur, this._curPsd);
 		}
 		this._doing = false;
 	}
 
-	async _doTransition(curIdx, newIdx, dir) {
-		if (0 === dir) {
-			const r = (curIdx < newIdx) ? newIdx - curIdx : newIdx + this._size - curIdx;
-			const l = (newIdx < curIdx) ? curIdx - newIdx : curIdx + this._size - newIdx;
+	async _doTransition(curIdx, curIdxPsd, idx, dir) {
+		if (0 === dir && curIdx !== idx) {
+			const r = (curIdx < idx) ? idx - curIdx : idx + this._size - curIdx;
+			const l = (idx < curIdx) ? curIdx - idx : curIdx + this._size - idx;
 			dir = (l < r) ? -1 : 1;
 		}
-		const ps = this._calcPosition(curIdx, dir);
+		let ps = this._calcPosition(curIdxPsd, dir);
 		for (let i = 0; i < this._sls.length; i += 1) {
 			this._sls[i].style.transition = '';
 			this._sls[i].style.transform  = `translateX(${ps[i] * 100}%)`;
 		}
-		await asyncTimeout(10).set();  // Wait
+		await asyncTimeout(100).set();  // Wait
 
 		let d = 0;
-		if (dir ===  1) d = newIdx - curIdx;
-		if (dir === -1) d = curIdx - newIdx;
+		if (dir ===  1) d = idx - curIdx;
+		if (dir === -1) d = curIdx - idx;
 		if (d < 0) d += this._size;
 
-		let curPs = ps;
+		let idxPsd = curIdxPsd;
 		for (let i = 0; i < d; i += 1) {
-			const tf = (d === 1) ? 'ease' : ((i === 0) ? 'ease-in' : ((i === d - 1) ? 'ease-out' : 'linear'));
-			[curPs, curIdx] = this._shift(curPs, curIdx, dir, this._time / d, tf);
+			[ps, idxPsd] = this._shift(ps, idxPsd, dir, this._time / d, this._getTransition(d, i));
 			await asyncTimeout(Math.floor(this._time * 1000 / d)).set();
 		}
-		return curIdx;
+		return [idx, idxPsd];
 	}
 
-	_shift(curPs, curIdx, dir, time, tf = 'ease') {
-		const len = this._sls.length;
-
-		let newIdx = curIdx + dir;
-		if (len - 1 < newIdx) newIdx = 0;
-		if (newIdx < 0) newIdx = len - 1;
-
-		const newPs = this._calcPosition(newIdx, dir);
-
-		for (let i = 0; i < len; i += 1) {
-			const t = (Math.abs(curPs[i] - newPs[i]) === 1) ? `transform ${time}s ${tf}` : '';
-			this._sls[i].style.transition = t;
-			this._sls[i].style.transform  = `translateX(${newPs[i] * 100}%)`;
+	_getTransition(d, i) {
+		if (d === 1) return 'ease';
+		if (d === 2) {
+			if (i === 0) return 'ease-in';
+			if (i === 1) return 'ease-out';
 		}
-		return [newPs, newIdx];
+		return 'linear';
 	}
 
-	_calcPosition(idx, dir) {
-		const len = this._sls.length;
-		const ps  = new Array(len);
-		ps[idx] = 0;
+	_shift(curPs, curIdxPsd, dir, time, tf = 'ease') {
+		const lenPsd = this._sls.length;
 
-		const hs = (dir !== -1) ? Math.ceil((len - 1) / 2) : Math.floor((len - 1) / 2);
-		const rs = len - 1 - hs;
+		let idxPsd = curIdxPsd + dir;
+		if (lenPsd - 1 < idxPsd) idxPsd = 0;
+		if (idxPsd < 0) idxPsd = lenPsd - 1;
+
+		const ps = this._calcPosition(idxPsd, dir);
+
+		for (let i = 0; i < lenPsd; i += 1) {
+			const t = (Math.abs(curPs[i] - ps[i]) === 1) ? `transform ${time}s ${tf}` : '';
+			this._sls[i].style.transition = t;
+			this._sls[i].style.transform  = `translateX(${ps[i] * 100}%)`;
+		}
+		return [ps, idxPsd];
+	}
+
+	_calcPosition(idxPsd, dir) {
+		const lenPsd = this._sls.length;
+		const ps     = new Array(lenPsd);
+
+		const hs = (dir !== -1) ? Math.ceil((lenPsd - 1) / 2) : Math.floor((lenPsd - 1) / 2);
+		const rs = lenPsd - 1 - hs;
+
 		for (let i = 1; i <= hs; i += 1) {
-			let j = idx + i;
-			if (len - 1 < j) j -= len;
+			let j = idxPsd + i;
+			if (lenPsd - 1 < j) j -= lenPsd;
 			ps[j] = i;
 		}
 		for (let i = 1; i <= rs; i += 1) {
-			let j = idx - i;
-			if (j < 0) j += len;
+			let j = idxPsd - i;
+			if (j < 0) j += lenPsd;
 			ps[j] = -i;
 		}
+		ps[idxPsd] = 0;
 		return ps;
 	}
 
