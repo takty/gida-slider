d59a4223991b7f5a3937fceb0b0639e231828988
Takuto Yanagida <yanagida@stxst.com>
2022-12-09 16:20:05 +0900

    Improve gulp process

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -10,7 +10,6 @@ import gulp from 'gulp';
 import { makeJsTask } from './gulp/task-js.mjs';
 import { makeSassTask } from './gulp/task-sass.mjs';
 import { makeCopyTask } from './gulp/task-copy.mjs';
-import { makeTimestampTask } from './gulp/task-timestamp.mjs';
 
 const js = gulp.parallel(
 	makeJsTask('./src/show.js', './dist/js'),
@@ -42,15 +41,20 @@ const doc_css = gulp.series(sass, makeCopyTask(['dist/css/*'], './docs/css'));
 
 const doc_sass = makeSassTask('docs/style.scss', './docs/css');
 
-const doc_timestamp = makeTimestampTask('docs/**/*.html', './docs');
+const doc_timestamp = async () => {
+	const { makeTimestampTask } = await import('./gulp/task-timestamp.mjs');
+	return makeTimestampTask('docs/**/*.html', './docs');
+}
+
+const fn = await doc_timestamp();
 
 const doc_watch = done => {
-	gulp.watch('src/**/*.js', gulp.series(doc_js, doc_timestamp));
-	gulp.watch('src/**/*.scss', gulp.series(doc_css, doc_timestamp));
-	gulp.watch('docs/style.scss', gulp.series(doc_sass, doc_timestamp));
+	gulp.watch('src/**/*.js', gulp.series(doc_js, fn));
+	gulp.watch('src/**/*.scss', gulp.series(doc_css, fn));
+	gulp.watch('docs/style.scss', gulp.series(doc_sass, fn));
 	done();
 };
 
-const doc_build = gulp.parallel(doc_js, doc_css, doc_sass, doc_timestamp);
+const doc_build = gulp.parallel(doc_js, doc_css, doc_sass, fn);
 
 export const doc = gulp.series(doc_build, doc_watch);
