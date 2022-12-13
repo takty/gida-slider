832a7536339dd659d1fa65a684330249b1ffb7fa
Takuto Yanagida <yanagida@stxst.com>
2022-12-13 17:16:57 +0900

    Update gulp process

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,61 +2,39 @@
  * Gulpfile
  *
  * @author Takuto Yanagida
- * @version 2022-12-09
+ * @version 2022-12-13
  */
+
+const WATCH_OPTS = { ignoreInitial: false, delay: 400 };
 
 import gulp from 'gulp';
 
 import { makeJsTask } from './gulp/task-js.mjs';
 import { makeSassTask } from './gulp/task-sass.mjs';
-import { makeCopyTask } from './gulp/task-copy.mjs';
 
-const js = gulp.parallel(
-	makeJsTask('./src/show.js', './dist/js'),
-	makeJsTask('./src/hero.js', './dist/js')
-);
-
-const sass = gulp.parallel(
-	makeSassTask('./src/show.scss', './dist/css'),
-	makeSassTask('./src/hero.scss', './dist/css')
-);
+const js   = makeJsTask('src/[^_]*.js', './dist/js');
+const sass = makeSassTask('src/[^_]*.scss', './dist/css');
 
 export const build = gulp.parallel(js, sass);
-
-const watch = done => {
-	gulp.watch('src/**/*.js', gulp.series(js));
-	gulp.watch('src/**/*.scss', gulp.series(sass));
-	done();
+export default () => {
+	gulp.watch('src/**/*.js', WATCH_OPTS, js);
+	gulp.watch('src/**/*.scss', WATCH_OPTS, sass);
 };
-
-export default gulp.series(build, watch);
 
 
 // -----------------------------------------------------------------------------
 
 
-const doc_js = gulp.series(js, makeCopyTask(['dist/js/*'], './docs/js'));
-
-const doc_css = gulp.series(sass, makeCopyTask(['dist/css/*'], './docs/css'));
-
-const doc_sass = makeSassTask('docs/style.scss', './docs/css');
-
-const doc_watch = async done => {
+export const doc = async () => {
+	const { makeCopyTask }      = await import('./gulp/task-copy.mjs');
 	const { makeTimestampTask } = await import('./gulp/task-timestamp.mjs');
+
+	const doc_js        = makeCopyTask('dist/js/*', './docs/js');
+	const doc_css       = makeCopyTask('dist/css/*', './docs/css');
+	const doc_sass      = makeSassTask('docs/style.scss', './docs/css');
 	const doc_timestamp = makeTimestampTask('docs/**/*.html', './docs');
 
-	gulp.watch('src/**/*.js', gulp.series(doc_js, doc_timestamp));
-	gulp.watch('src/**/*.scss', gulp.series(doc_css, doc_timestamp));
-	gulp.watch('docs/style.scss', gulp.series(doc_sass, doc_timestamp));
-	done();
+	gulp.watch('src/**/*.js', WATCH_OPTS, gulp.series(js, doc_js, doc_timestamp));
+	gulp.watch('src/**/*.scss', WATCH_OPTS, gulp.series(sass, doc_css, doc_timestamp));
+	gulp.watch('docs/style.scss', WATCH_OPTS, gulp.series(doc_sass, doc_timestamp));
 };
-
-const doc_build = async done => {
-	const { makeTimestampTask } = await import('./gulp/task-timestamp.mjs');
-	const doc_timestamp = makeTimestampTask('docs/**/*.html', './docs');
-
-	gulp.parallel(doc_js, doc_css, doc_sass, doc_timestamp);
-	done();
-};
-
-export const doc = gulp.series(doc_build, doc_watch);
